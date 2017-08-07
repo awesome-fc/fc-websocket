a0e7c9da4786309327cbb19d2474d89c518a8327
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2017-08-07 20:00:34 +0800

    implement web shell atop FunctionCompute

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-# React Term
+# Give me a shell, I can...
 
-A couple of days back, I got an email from Columbia (the university that I'm stated to join) informing me that my new email ID and other student IT services were ready. Hosting my own webpage on a university's domain had long been a wish of mine, so as soon as I learnt about having some server space on the university's server I got excited wanted to put something interesting. Since I already have a boring [about me](http://prakhar.me/about) page, I went with something different and built a simple terminal emulator in [React](http://facebook.github.io/react/)!
+Instantly run a linux shell in the cloud as your need, discard it when
+you're done.
+
+Powered by Aliyun [FunctionCompute](https://www.aliyun.com/product/fc)
 
 ![img](shot.png)
 
-Demo - http://columbia.edu/~ps2894
+Demo - http://fc-public.oss-cn-hangzhou.aliyuncs.com/demo/shell/index.html
 
 ### Run
+
 ```
 $ npm install
 $ npm run dist
+$ open index.html
 ```
 
 ### Dev
+
 ```
 $ npm install
 $ npm run watch
--- src/app.js
+++ src/app.js
@@ -1,98 +1,75 @@
 var React = require('react');
+var axios = require('axios');
+
+const Prompt = 'fc@aliyun $ ';
+const ShellApi = 'http://api.rockuw.com/shell';
 
 var App = React.createClass({
   getInitialState: function() {
-      return {
-        commands: {},
-        history: [],
-        prompt: '$ '
-      }
+    return {
+      history: [],
+      prompt: Prompt,
+    }
   },
   clearHistory: function() {
-      this.setState({ history: [] });
+    this.setState({ history: [] });
   },
-  registerCommands: function() {
-    this.setState({
-      commands: {
-        'clear' : this.clearHistory,
-        'ls'    : this.listFiles,
-        'intro' : this.showWelcomeMsg,
-        'help'  : this.showHelp,
-        'cat'   : this.catFile,
-        'source': this.openLink('https://github.com/prakhar1989/react-term/blob/master/src/app.js'),
-        'github': this.openLink('http://github.com/prakhar1989'),
-        'blog'  : this.openLink('http://prakhar.me'),
-        'resume': this.openLink('https://github.com/prakhar1989/cv/blob/master/Resume.pdf')
+  execShellCommand: function(cmd) {
+    var that = this;
+    that.setState({'prompt': ''})
+    axios.get(ShellApi+'?cmd='+cmd).then(function (res) {
+      console.log(res);
+      res.data.split('\n').forEach(function(line) {
+        that.addHistory(line);
+      });
+      that.setState({'prompt': Prompt})
+    }).catch(function(err) {
+      var errText = '';
+      if (err.response) {
+        // TODO should print the request id, however api gateway
+        // doesn't support it yet
+        errText = err.response.status + ' ' + err.response.statusText
+      } else {
+        errText = err.toString();
       }
+      that.addHistory(errText);
+      that.setState({'prompt': Prompt})
     });
   },
-  listFiles: function() {
-      this.addHistory("README.md");
-  },
   showWelcomeMsg: function() {
-      this.addHistory("Hello, I'm Prakhar Srivastav, a graduate student in the Computer Science department (Machine Learning track).");
-      this.addHistory("Type `help` to see what all commands are available");
-  },
-  catFile: function(arg) {
-      if (arg === "README.md") {
-          this.addHistory('### REACT TERM');
-          this.addHistory("A couple of days back, I got an email from Columbia (the university that I'm stated to join) informing me that my new email ID and other student IT services were ready. Hosting my own webpage on a university's domain had long been a wish of mine, so as soon as I learnt about having some server space on the university's server I got excited wanted to put something interesting. Since I already have " +
-                          "a boring about me page, I went " +
-                          "with something different and built a simple terminal emulator in React!");
-          this.addHistory("type `source` to view the source code");
-      } else {
-          this.addHistory("cat: " +  arg + ": No such file or directory");
-      }
+    this.addHistory('Welcome to FunctionCompute! Have fun!');
   },
   openLink: function(link) {
-      return function() {
-        window.open(link, '_blank');
-      }
-  },
-  showHelp: function() {
-      this.addHistory("help - this help text");
-      this.addHistory("github - view my github profile");
-      this.addHistory("source - browse the code for this page");
-      this.addHistory("intro - print intro message");
-      this.addHistory("blog - read some stuff that I've written");
-      this.addHistory("clear - clear screen");
-      this.addHistory("cat - print contents of a file");
-      this.addHistory("ls - list files");
-      this.addHistory("resume - view my resume");
+    return function() {
+      window.open(link, '_blank');
+    }
   },
   componentDidMount: function() {
-      var term = this.refs.term.getDOMNode();
+    var term = this.refs.term.getDOMNode();
 
-      this.registerCommands();
-      this.showWelcomeMsg();
-      term.focus();
+    this.showWelcomeMsg();
+    term.focus();
   },
   componentDidUpdate: function() {
-      var el = React.findDOMNode(this);
-      //var container = document.getElementsByClassName('container')[0];
-      var container = document.getElementById("main");
-      container.scrollTop = el.scrollHeight;
+    var el = React.findDOMNode(this);
+    //var container = document.getElementsByClassName('container')[0];
+    var container = document.getElementById("main");
+    container.scrollTop = el.scrollHeight;
   },
   handleInput: function(e) {
-      if (e.key === "Enter") {
-          var input_text = this.refs.term.getDOMNode().value;
-          var input_array = input_text.split(' ');
-          var input = input_array[0];
-          var arg = input_array[1];
-          var command = this.state.commands[input];
+    if (e.key === "Enter") {
+      var input_text = this.refs.term.getDOMNode().value;
+      var input_array = input_text.split(' ');
+      var input = input_array[0];
+      var arg = input_array[1];
 
-          this.addHistory(this.state.prompt + " " + input_text);
-
-          if (command === undefined) {
-              this.addHistory("sh: command not found: " + input);
-          } else {
-              command(arg);
-          }
-          this.clearInput();
-      }
+      this.addHistory(this.state.prompt + " " + input_text);
+      this.execShellCommand(input_text);
+      this.clearInput();
+    }
   },
   clearInput: function() {
-      this.refs.term.getDOMNode().value = "";
+    this.refs.term.getDOMNode().value = "";
   },
   addHistory: function(output) {
     var history = this.state.history;
@@ -106,18 +83,18 @@ var App = React.createClass({
     term.focus();
   },
   render: function() {
-      var output = this.state.history.map(function(op, i) {
-          return <p key={i}>{op}</p>
-      });
-      return (
-        <div className='input-area' onClick={this.handleClick}>
-          {output}
-          <p>
-            <span className="prompt">{this.state.prompt}</span> 
-            <input type="text" onKeyPress={this.handleInput} ref="term" />
-          </p>
-        </div>
-      )
+    var output = this.state.history.map(function(op, i) {
+      return <p key={i}>{op}</p>
+    });
+    return (
+      <div className='input-area' onClick={this.handleClick}>
+        {output}
+        <p>
+          <span className="prompt">{this.state.prompt}</span>
+          <input type="text" onKeyPress={this.handleInput} ref="term" />
+        </p>
+      </div>
+    )
   }
 });
 
